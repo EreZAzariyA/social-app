17b313232bd9f8239a66632bb5267ff305fcc684
Erez Azariya <erezazariya@gmail.com>
2023-02-04 19:25:09 +0200

    friends work !

--- imports/ui/components/friend-profile/index.js
+++ imports/ui/components/friend-profile/index.js
@@ -8,44 +8,59 @@ import {FiUserPlus,FiUserX, FiUserCheck} from "react-icons/fi";
 import { FriendsRequests } from "../../../api/friends-requests/friends-requests";
 import { BiUserCheck, BiUserX } from "react-icons/bi";
 import "./style.css";
-import { FriendsDB } from "../../../api/friends/friends";
 
 const UserProfile = ()=>{
   const params = useParams();
   const navigate = useNavigate();
   const [toConfirm,setToConfirm] = useState(false);
 
-  const {friendProfileIsReady, friend, relationshipStatus, requests} = useTracker(()=>{
+  const {friendProfileIsReady, friend, relationshipStatus, friendsRequests} = useTracker(()=>{
     const subscribe = Meteor.subscribe('friends-requests.user');
+    
+    // The user who currently displayed on the browser:
     const friendId = params.user_id;
     const currentUser = Meteor.users.findOne({_id:friendId});
-    const allRequests = FriendsRequests.find({friend_id:Meteor.userId()}).fetch()
+
+    // Get only the requests that sent to me:
+    const allRequests = FriendsRequests.find({friend_id:Meteor.userId()}).fetch();
+    
+    // To figure out the request details:
     const status = FriendsRequests.find(
-      { user_id: Meteor.userId(), friend_id: friendId },
-      { fields: { 'details': 1 } }).fetch();
+      {
+        $or: [
+          { user_id: Meteor.userId(), friend_id: friendId },
+          { user_id: friendId, friend_id: Meteor.userId()}
+        ]
+      },
+      {
+        fields: { 'details': 1 }
+      }
+      ).fetch();
 
       return{
         friendProfileIsReady: subscribe.ready(),
         friend:currentUser,
         relationshipStatus:status[0],
-        requests: allRequests
+        friendsRequests: allRequests
       }
     },[params.user_id]);
 
   useEffect(()=>{
     if(params.user_id === Meteor.userId()){
       navigate('/profile');
-    }
-    requests?.map((request)=>{
-      if(request.user_id === friend._id){
-        setToConfirm(true);
-        console.log(request);
-      }else{
-        setToConfirm(false);
-      }
-    });
+    };
 
-  },[params.user_id,requests,friend]);
+    // If the request has been sent to me, need to confirm the request
+    friendsRequests?.map((request)=>{
+      if(request.details.status === RelationshipSteps.REQUESTֹ_SENT){
+        setToConfirm(true);
+        console.log("need to confirm");
+      }
+      console.log(request);
+    });
+  });
+
+// console.log(relationshipStatus);
 
   const sendFriendRequest = () =>{
     Meteor.call('friends.requests.send', friend, err =>{
@@ -53,69 +68,114 @@ const UserProfile = ()=>{
       alert('request has been sent');
     })
   };
-  
   const cancelFriendRequest = ()=>{
     Meteor.call('friends.requests.cancel', friend, err =>{
       if(err) return alert(err);
       alert('request has been canceled');
     })
-  }
-
-  const approveRequest = (userToApprove)=>{
-    Meteor.call('friends.requests.approve',userToApprove,err=>{
+  };
+  const approveRequest = ()=>{
+    Meteor.call('friends.requests.approve',friend,err=>{
       if(err){
         return console.log(err);
       }
       alert('accepted!');
     })
-  }
-
-  const rejectRequest = (userToReject)=>{
-    Meteor.call('friends.requests.reject',userToReject,err=>{
+  };
+  const rejectRequest = ()=>{
+    Meteor.call('friends.requests.reject',friend,err=>{
       if(err){
         return console.log(err);
       }
       alert('rejected!');
     });
-  }
-
+  };
 
   if(friendProfileIsReady){
     if(!friend) return <h1>User not found</h1>
+
     return(
       <>
         <h1>{friend.profile.first_name}</h1>
-        {
-          !relationshipStatus
-          ?
-            toConfirm
-            ?
-            <div className="to_confirm_container">
-              <Tooltip title="Approve">
-                <Button shape="circle" icon={<BiUserCheck/>} onClick={()=>approveRequest}/>
-              </Tooltip>
-              <Tooltip title="Denied">
-                <Button shape="circle" danger icon={<BiUserX/>} onClick={()=>rejectRequest}/>
-              </Tooltip>
-            </div>
-            :
-            <Tooltip title="Send-Request">
-              <Button shape="circle" icon={<FiUserPlus/>} onClick={sendFriendRequest}/>
-            </Tooltip>
-          :
-            relationshipStatus.details
-              &&
+          <>
+            {!relationshipStatus 
+              ?
+                <Tooltip title="Send-Request">
+                  <Button shape="circle" icon={<FiUserPlus/>} onClick={sendFriendRequest}/>
+                </Tooltip>
+              :
                 <>
-                  {relationshipStatus.details.status === RelationshipSteps.REQUESTֹ_SENT &&
-                    <Tooltip title="Cancel-Request">
-                    <Button shape="circle" danger icon={<FiUserX/>} onClick={cancelFriendRequest}/>
-                  </Tooltip>
+                  {relationshipStatus.details.status === RelationshipSteps.REQUESTֹ_SENT
+                    &&
+                    <>
+                      {!toConfirm 
+                      ?
+                        <Tooltip title="Cancel-Request">
+                          <Button shape="circle" danger icon={<FiUserX/>} onClick={cancelFriendRequest}/>
+                        </Tooltip>
+                      :
+                        <div className="to_confirm_container">
+                          <Tooltip title="Approve">
+                            <Button shape="circle" icon={<BiUserCheck/>} onClick={approveRequest}/>
+                          </Tooltip>
+                          <Tooltip title="Denied">
+                            <Button shape="circle" danger icon={<BiUserX/>} onClick={rejectRequest}/>
+                          </Tooltip>
+                        </div>}
+                    </>
+
                   }
                   {relationshipStatus.details.status === RelationshipSteps.APPROVED &&
                     <Button shape="circle" icon={<FiUserCheck/>}/>
                   }
                 </>
-        }
+            }
+            {/* {!relationshipStatus &&
+              <Tooltip title="Send-Request">
+                <Button shape="circle" icon={<FiUserPlus/>} onClick={sendFriendRequest}/>
+              </Tooltip>
+            } */}
+
+{/* 
+
+            {relationshipStatus.details.status === RelationshipSteps.REQUESTֹ_SENT &&
+              <Tooltip title="Cancel-Request">
+                <Button shape="circle" danger icon={<FiUserX/>} onClick={cancelFriendRequest}/>
+              </Tooltip>
+            }
+            {relationshipStatus.details.status === RelationshipSteps.APPROVED &&
+              <Button shape="circle" icon={<FiUserCheck/>}/>
+            }
+ */}
+
+          </>
+        
+        
+
+
+
+        {/* {
+          relationshipStatus
+          ?
+          toConfirm
+          ?
+          <div className="to_confirm_container">
+            <Tooltip title="Approve">
+              <Button shape="circle" icon={<BiUserCheck/>} onClick={()=>approveRequest}/>
+            </Tooltip>
+            <Tooltip title="Denied">
+              <Button shape="circle" danger icon={<BiUserX/>} onClick={()=>rejectRequest}/>
+            </Tooltip>
+          </div>
+          :
+          <Tooltip title="Cancel-Request">
+            <Button shape="circle" danger icon={<FiUserX/>} onClick={cancelFriendRequest}/>
+          </Tooltip>
+          :
+          <Tooltip title="Send-Request">
+            <Button shape="circle" icon={<FiUserPlus/>} onClick={sendFriendRequest}/>
+          </Tooltip>
+        } */}
       </>
     );
   }else return <Spin/>
